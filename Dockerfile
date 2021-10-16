3913f7af481b8e38baf4392856537d3102a3e82c
zdmc23 <191707+zdmc23@users.noreply.github.com>
2021-10-15 21:48:39 -0400

    mirror aws and gcp services as closely as possible

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,4 @@ RUN go build -o /main
 # copy artifacts to a clean image
 FROM public.ecr.aws/lambda/provided:al2
 COPY --from=build /main /main
-ENTRYPOINT [ "/main" ] 
+ENTRYPOINT [ "/main" ]
--- main.go
+++ main.go
@@ -1,28 +1,39 @@
 package main
- 
+
 import (
+	"encoding/base64"
 	"os/exec"
-  "github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type Output struct {
-  Payload string `json:"payload"`
+type Response struct {
+	Output string `json:"output"`
 }
 
-type Input struct {
-	Cmd string
-	Nonce string
+type Request struct {
+	Input string `json:"input"`
 }
- 
-func HandleLambdaEvent(input Input) (Output, error) {
-	cmd := exec.Command("bash", "-c", input.Cmd)
+
+func HandleLambdaEvent(req Request) (Response, error) {
+	// Base64 Decode Input
+	inputDec, inputErr := base64.StdEncoding.DecodeString(req.Input)
+	if inputErr != nil {
+		panic(inputErr)
+	}
+	// Exec Command
+	//cmd := exec.CommandContext(r.Context(), "bash", "-c", string(inputDec))
+	cmd := exec.Command("bash", "-c", string(inputDec))
 	cmdOut, cmdErr := cmd.Output()
 	if cmdErr != nil {
 		panic(cmdErr)
 	}
-  return Output{string(cmdOut)}, nil
+	// Base64 Encode Output
+	outputEnc := base64.StdEncoding.EncodeToString([]byte(cmdOut))
+	// HTTP Response
+	return Response{string(outputEnc)}, nil
 }
- 
+
 func main() {
-  lambda.Start(HandleLambdaEvent)
+	lambda.Start(HandleLambdaEvent)
 }
